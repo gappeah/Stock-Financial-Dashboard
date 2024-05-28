01afd4ac70851c3e97c2fc60b8d2db1106983d27
George <appeahg@outlook.com>
2024-05-28 22:45:22 +0100

    Create Main.py and establish imports

---
+++ Main.py
@@ -0,0 +1,13 @@
+# Import libraies and modules
+import numpy as np
+import pandas as pd
+
+import math
+import datetime as dt
+
+from bokeh.io import curdoc
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, Select
+from bokeh.plotting import figure
+from bokeh.palettes import Spectral6
+
