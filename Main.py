a7daa82b339ac01397a92a89db2cd00559521daa
George <appeahg@outlook.com>
2024-05-29 12:27:40 +0100

    commit to main

--- Main.py
+++ Main.py
@@ -32,4 +32,9 @@ stock2_text = TextInput(title="Stock 2")
 date_picker_from = DatePicker(title="Start Date", value="2020-01-01", min_date="2000-01-01", max_date=dt.datetime.now().strftime("%Y-%m-%d"))
 date_picker_to = DatePicker(title="End Date", value="2020-02-01", min_date="2000-01-01", max_date=dt.datetime.now().strftime("%Y-%m-%d"))
 
-indicator_choice = MultiChoice(options=["100 Day SMA", "30 Day SMA", "Linear Regression"], value=["100 Day SMA", "30 Day SMA"])
+indicator_choice = MultiChoice(options=["100 Day SMA", "30 Day SMA", "Linear Regression"])
+
+
+load_button = Button(label="Load Data", button_type="success")
+
+load_button.on_click = (lambda(label="Load Data", button_type="success")
